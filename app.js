9a0bf97ea8ebfa90bf640d98f330c11c1c66360d
rnjai <rnjailamba12@gmail.com>
2015-12-02 11:26:19 +0530

    making app.js more informative

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var users = require('./routes/users');
 var flash = require('connect-flash');
 var session = require('express-session');
 var mongoose = require('mongoose');
+var passport = require('passport');
+
 var fs = require("fs");
 
 mongoose.connect('mongodb://localhost/users', function (error) {
@@ -37,7 +39,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');// set up ejs for templating
 
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
@@ -45,20 +47,24 @@ app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// required for passport
+
 app.use(session({
     secret: 'keyboard cat',
     proxy: true,
     resave: true,
     saveUninitialized: true
-}));
-//https://github.com/expressjs/session/issues/56
-app.use(flash());
+}));//https://github.com/expressjs/session/issues/56
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 
-app.use(logger('dev')); 
-app.use(bodyParser.json());
+app.use(logger('dev')); // log every request to the console
+app.use(bodyParser.json());// get information from html forms
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser());// read cookies (needed for auth)   
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
