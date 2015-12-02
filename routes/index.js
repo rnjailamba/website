1b9e015118ed528355aa497fefc908cc40c25738
rnjai <rnjailamba12@gmail.com>
2015-12-02 08:21:40 +0530

    done with index and about pages

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,28 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	// console.log(__dirname);
-  res.render('basicPages/index', { title: 'Express' });
+  var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+    res.render('basicPages/index', {
+        drinks: drinks,
+        tagline: tagline
+    });
+
 });
 
 /* GET home page. */
 router.get('/index', function(req, res, next) {
   res.render('basicPages/index', { title: 'Express' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('basicPages/about', { title: 'Express' });
 });
 
 module.exports = router;
