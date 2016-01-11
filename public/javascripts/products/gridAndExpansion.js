df18899047b8357c8984372b77349ee00d8dec1d
rnjai <rnjailamba12@gmail.com>
2016-01-11 13:25:39 +0530

    removing the arrow from pdp link

--- public/javascripts/products/gridAndExpansion.js
+++ public/javascripts/products/gridAndExpansion.js
@@ -343,7 +343,7 @@ var Grid = (function() {
 			// create Preview structure:
 			this.$title = $( '<h3></h3>' );
 			this.$description = $( '<p></p>' );
-			this.$href = $( '<a href="#">Visit website</a>' );
+			this.$href = $( '<a class="pdpLink" href="#">View Product Details</a>' );
 			this.$details = $( '<div class="og-details"></div>' ).append( this.$title, this.$description, this.$href );
 			this.$loading = $( '<div class="og-loading"></div>' );
 			this.$photos = $( '<div class="gallery-cell" style="background:#3498db"></div><div class="gallery-cell" style="background:#ea5e50"></div><div class="gallery-cell" style="background:#f1c40f"></div>' );
