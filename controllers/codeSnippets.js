4829751c913543fe053f8aa411e55e510413f3bf
rnjai <rnjailamba12@gmail.com>
2016-02-03 09:40:31 +0530

    codeSnip - adding image upload snippet

---
+++ controllers/codeSnippets.js
@@ -0,0 +1,18 @@
+var modules = require('./setup/all_modules');//require all modules that are shared by all controllers
+var router = modules.express.Router();
+var config = require('../config/config.js');//require all modules that are shared by all controllers
+
+
+// INDEX
+// ==============================================
+router.get('/fileUpload', function(req, res, next) {
+  res.render('codeSnippets/fileUpload', { title: 'Cementify Blog' });
+});
+
+
+
+var justPrintSomething = function(){
+    console.log("print something");
+}
+
+module.exports.router = router;
--- controllers/setup/initialize.js
+++ controllers/setup/initialize.js
@@ -3,9 +3,11 @@ var basicPages = require('../index.js');
 var users1 = require('../users1.js');
 var blog = require('../blog.js');
 var products = require('../products.js');
+var codeSnippets = require('../codeSnippets.js');
 
 exportedApp.app.use('/', basicPages);
 exportedApp.app.use('/users1', users1.router);
 exportedApp.app.use('/blog', blog.router);
 exportedApp.app.use('/products', products);
+exportedApp.app.use('/codeSnippets', codeSnippets.router);
 
